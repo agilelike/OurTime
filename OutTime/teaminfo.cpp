c7e2bb3a4d52faa43dfa7fd5d754600311702d38
hitGeneral <791107311@qq.com>
2019-06-30 20:54:58 +0800

    message1

--- OutTime/teaminfo.cpp
+++ OutTime/teaminfo.cpp
@@ -1,6 +1,8 @@
 #include "teaminfo.h"
 #include "ui_teaminfo.h"
-
+#include "user.h"
+#include <QString>
+#include "message.h"
 teaminfo::teaminfo(QWidget *parent):
     QWidget(parent),
     ui(new Ui::teaminfo)
@@ -9,9 +11,41 @@ teaminfo::teaminfo(QWidget *parent):
     hide();
     //ui->scrollAreaWidgetContents->setStyleSheet("background-color: rgb(255, 255, 0);");
     ui->setupUi(this);
+    QList<Message>mes;//其实是public类型的
 }
 
 teaminfo::~teaminfo()
 {
     delete ui;
 }
+
+int teaminfo::findID(QString username)
+{
+    //根据用户名找用户ID
+//    int id;
+//    return id;
+}
+
+void teaminfo::on_pushButton_clicked()
+{
+    QString context = ui->textEdit->toPlainText();
+    QString toname = ui->comboBox->currentText();
+    int toID = findID(toname);
+    user->sendMessage(toID,context);
+
+}
+
+void teaminfo::judge()
+{
+    if(ui->checkBox->isChecked()==true)
+    {
+        //random
+    }
+}
+
+void teaminfo::showmessage()
+{
+
+}
+
+
--- OutTime/teaminfo.h
+++ OutTime/teaminfo.h
@@ -15,6 +15,9 @@ public:
     explicit teaminfo(QWidget *parent = 0);
     ~teaminfo();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::teaminfo *ui;
 };
