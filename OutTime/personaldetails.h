b0d5e02998d771c3e0ba7ba979130e791fff0f23
CBB0403 <51751407+CBB0403@users.noreply.github.com>
2019-07-01 18:03:31 +0800

    4
    折线图

--- OutTime/personaldetails.h
+++ OutTime/personaldetails.h
@@ -2,6 +2,8 @@
 #define PERSONALDETAILS_H
 
 #include <QDialog>
+#include<QPaintEvent>
+#include<QtGui>
 
 namespace Ui {
 class personalDetails;
@@ -16,6 +18,8 @@ public:
     int state = 0;
     void haveTeam(int state);
     ~personalDetails();
+    void PaintImage1();
+    void PaintImage2();
 
 private slots:
     void on_pushButton_3_clicked();
@@ -32,12 +36,22 @@ private slots:
 
     void on_pushButton_clicked();
 
+
+protected:
+    void paintEvent(QPaintEvent *){
+        QPainter painter(this);
+        painter.drawImage(20,200,image1);
+        painter.drawImage(310,200,image2);
+    }
+
 signals:
     void showInformation(int state,int create);
     void showCreateTeam(int state);
 
 private:
     Ui::personalDetails *ui;  
+    QImage image1;
+    QImage image2;
 };
 
 #endif // PERSONALDETAILS_H
